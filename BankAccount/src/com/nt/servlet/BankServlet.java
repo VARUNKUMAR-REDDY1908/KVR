44fc930daac9caf885a88c1bc3a56a04f484489e
varunreddy <varunreddy1921@gmail.com>
2020-08-05 19:00:10 +0530

    initialised condition

--- BankAccount/src/com/nt/servlet/BankServlet.java
+++ BankAccount/src/com/nt/servlet/BankServlet.java
@@ -9,14 +9,15 @@ public class BankServlet extends HttpServlet{
 	@Override
 	public void doPost(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException{
 		PrintWriter pw=null;
-		String name=null;int balance=0;
+		String name=null;
+		int balance=0;
 				int age=0;
 		pw=res.getWriter();
 		res.setContentType("text/html");
 		name=req.getParameter("pname");
 		age=Integer.parseInt(req.getParameter("page"));
 		balance=Integer.parseInt(req.getParameter("Amount"));
-	if(balance>=1000&&age>=18){
+	if(balance>=500&&age>=18){
 		pw.println("<h1 style='color:green;text-align:center'>Mr. " + name + " You Are Eligible For Opening Account</h1>");
 		
 	}
